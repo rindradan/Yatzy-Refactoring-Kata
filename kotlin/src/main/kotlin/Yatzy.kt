0d729c8294f9046dffa96ae58d67229edf0bf530
rindra randriambololona <rindra.randriambololona@gmail.com>
2024-05-01 01:55:44 +0200

    refacto functions

--- kotlin/src/main/kotlin/Yatzy.kt
+++ kotlin/src/main/kotlin/Yatzy.kt
@@ -83,31 +83,26 @@ class Yatzy {
 
     fun onePair(vararg dices: Int): Int {
         val tallies = getTalliesByDice(dices)
-        return if (hasPairs(tallies)) {
-            tallies.filter { it.value >= 2 }.keys.map { it + 1 }.map { it * 2 }.max()
-        } else 0
+        return tallies.countSameNumber(2)
     }
 
     fun twoPairs(vararg dices: Int): Int {
         val tallies = getTalliesByDice(dices)
-        return if (hasTwoPairs(tallies)) {
-            tallies.filter { it.value >= 2 }.keys.map { it + 1 }.map { it * 2 }.sum()
-        } else 0
+        return tallies.filter { it.value >= 2 }.takeIf { it.count() >= 2 }?.keys?.sumOf { it * 2 } ?: 0
     }
 
     fun threeOfAKind(vararg dices: Int): Int {
         val tallies = getTalliesByDice(dices)
-        return if (hasThreeOfAKindOrMore(tallies)) {
-            tallies.filter { it.value >= 3 }.keys.map { it + 1 }.map { it * 3 }.max()
-        } else 0
+        return tallies.countSameNumber(3)
     }
 
     fun fourOfAKind(vararg dices: Int): Int {
         val tallies = getTalliesByDice(dices)
-        return if (hasFourOfAKindOrMore(tallies)) {
-            tallies.filter { it.value >= 4 }.keys.map { it + 1 }.map { it * 4 }.max()
-        } else 0
+        return tallies.countSameNumber(4)
     }
+
+    private fun Map<Int, Int>.countSameNumber(number: Int) =
+        filter { it.value >= number }.keys.maxOfOrNull { it * number } ?: 0
 
     fun smallStraight(vararg dices: Int): Int {
         val tallies = getTalliesByDice(dices)
@@ -121,27 +116,25 @@ class Yatzy {
 
     fun fullHouse(vararg dices: Int): Int {
         val tallies = getTalliesByDice(dices)
-        return if (hasOnePair(tallies) && hasThreeOfAKind(tallies)) {
-            dices.sum()
-        } else 0
+        return if (isFullHouse(tallies)) dices.sum() else 0
     }
 
-    private fun getTalliesByDice(dices: IntArray): Map<Int,Int> {
-        val tallies = HashMap<Int,Int>(6)
+    private fun getTalliesByDice(dices: IntArray): Map<Int, Int> {
+        val tallies = HashMap<Int, Int>(6)
         for (dice in dices) {
-            tallies[dice - 1] = (tallies[dice - 1] ?: 0) + 1
+            tallies[dice] = (tallies[dice] ?: 0) + 1
         }
         return tallies
     }
 
-    private fun hasOnePair(tallies: Map<Int,Int>): Boolean = tallies.any { it.value == 2 }
-    private fun hasPairs(tallies: Map<Int,Int>): Boolean = tallies.any { it.value >= 2 }
-    private fun hasTwoPairs(tallies: Map<Int,Int>): Boolean = tallies.count { it.value >= 2 } >= 2
-    private fun hasThreeOfAKind(tallies: Map<Int,Int>): Boolean = tallies.any { it.value == 3 }
-    private fun hasThreeOfAKindOrMore(tallies: Map<Int,Int>): Boolean = tallies.any { it.value >= 3 }
-    private fun hasFourOfAKindOrMore(tallies: Map<Int,Int>): Boolean = tallies.any { it.value >= 4 }
-    private fun hasSmallStraight(tallies: Map<Int,Int>): Boolean =
+    private fun isFullHouse(tallies: Map<Int, Int>): Boolean =
+        hasOnePair(tallies) && hasThreeOfAKind(tallies)
+
+    private fun hasOnePair(tallies: Map<Int, Int>): Boolean = tallies.any { it.value == 2 }
+    private fun hasThreeOfAKind(tallies: Map<Int, Int>): Boolean = tallies.any { it.value == 3 }
+    private fun hasSmallStraight(tallies: Map<Int, Int>): Boolean =
         tallies.filterKeys { it in 0..4 }.all { it.value == 1 }
-    private fun hasLargeStraight(tallies: Map<Int,Int>): Boolean =
+
+    private fun hasLargeStraight(tallies: Map<Int, Int>): Boolean =
         tallies.filterKeys { it in 1..5 }.all { it.value == 1 }
 }
